9467a6a87d34ce7d5bc29207ea9b5548597eebd0
fedorzaharov <f.zakharov302@gmail.com>
2025-09-17 20:29:41 +0300

    Trying to successfully test pylint #2:
    edited "statues/hw7_solution.py": added "for....."

--- homeworks/hw7/statues/hw7_solution.py
+++ homeworks/hw7/statues/hw7_solution.py
@@ -1,21 +1,16 @@
 def missing_statues(sizes):
-    if len(sizes) == 0:
+    if not sizes:
         return 0
 
     min_size = sizes[0]
     max_size = sizes[0]
 
     for size in sizes:
-        if size < min_size:
-            min_size = size
-        if size > max_size:
-            max_size = size
+        min_size = min(min_size, size)
+        max_size = max(max_size, size)
 
     total_statues = max_size - min_size + 1
-
-    unique_sizes = set()
-    for size in sizes:
-        unique_sizes.add(size)
+    unique_sizes = set(sizes)
 
     missing = total_statues - len(unique_sizes)
     return missing
