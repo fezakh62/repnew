49c7143b720d5b047c0ab13d0fff9bb17c139852
fedorzaharov <f.zakharov302@gmail.com>
2025-09-18 10:47:58 +0300

    removed empty lines from bulls_and_cows

--- homeworks/hw7/bulls_and_cows/hw7_solution.py
+++ homeworks/hw7/bulls_and_cows/hw7_solution.py
@@ -2,20 +2,18 @@ import random
 
 
 def generate_secret_number():
-
-
     digits = list("0123456789")
     random.shuffle(digits)
     return ''.join(digits[:4])
 
 
-def check_guess(SECRET, guess):
+def check_guess(secret, guess):
     bulls = 0
     cows = 0
     for i in range(4):
-        if guess[i] == SECRET[i]:
+        if guess[i] == secret[i]:
             bulls += 1
-        elif guess[i] in SECRET:
+        elif guess[i] in secret:
             cows += 1
     return bulls, cows
 
